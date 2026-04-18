c1478e8d529aad95b5133ea62ab722870218a667
yakisugita <yakisugita@gmail.com>
2026-04-19 03:38:53 +0900

    ログインボタン設置,ログイン処理(未完成)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// トップページ
+app.use('/', serveStatic({ path: './static/index.html' }))
+app.use('/*', serveStatic({ root: './static/' }))
+
+// ログインする度にアクセス、もしDBに登録がなかったらDBにユーザーを登録する
+app.get('/signin', (c) => {
+  return c.text('')
+})
+
+// ユーザー削除
+
+// タスク一覧
+app.get('/api/v1/tasks', (c) => {
+  const userId = c.req.query('userId')
+  
+  // jsonでタスクのリストを返す
+  return c.json({})
 })
 
 export default app
